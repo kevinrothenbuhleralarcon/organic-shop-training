783b345d7e7526fc4668527e9021c3ef3c67c607
Deadlye <kevin.rothenbuhler@gmail.com>
2022-10-09 13:11:03 +0200

    Add google authentication

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  username: String | null | undefined;
 
-  ngOnInit(): void {
+  constructor(public auth: AngularFireAuth) { 
+    auth.authState.subscribe(result => {
+      this.username = result?.displayName;
+    })
   }
 
+  logout() {
+    this.auth.signOut();
+  }
 }
