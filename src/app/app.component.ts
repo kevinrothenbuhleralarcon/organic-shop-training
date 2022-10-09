d74baf308a9c361affdd021cf10ef8aad8461b32
Deadlye <kevin.rothenbuhler@gmail.com>
2022-10-09 17:46:32 +0200

    Add an auth guard for unsubscribed users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'oshop';
+export class AppComponent implements OnDestroy {
+
+  private subscription: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.subscription = this.authService.user$.subscribe(user => {
+      if(user) {        
+        const url = localStorage.getItem("returnUrl");        
+        this.router.navigate([url]);
+      }
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Observable } from 'rxjs';
 import firebase from "firebase/compat/app";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,16 @@ export class AuthService {
 
   user$: Observable<firebase.User | null>;
 
-  constructor(private auth: AngularFireAuth) {
+  constructor(
+    private auth: AngularFireAuth, 
+    private route: ActivatedRoute
+  ) {
     this.user$ = auth.authState;
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || "/";
+    localStorage.setItem("returnUrl", returnUrl);
     this.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
